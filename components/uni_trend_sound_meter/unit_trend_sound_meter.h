09ba478c14323a5401086b798a00e772998b280c
Dawei Zhang <daweiz@live.com>
2022-06-12 21:45:29 +1000

    fixup!

--- components/uni_trend_sound_meter/unit_trend_sound_meter.h
+++ components/uni_trend_sound_meter/unit_trend_sound_meter.h
@@ -19,8 +19,16 @@ espbt::ESPBTUUID uuid128_from_string(std::string value);
 class UnitTrendSoundMeter : public Component, public BLEClientNode {
  public:
   UnitTrendSoundMeter() : Component(){};
-//   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
-//                            esp_ble_gattc_cb_param_t *param) override;
+
+//   float get_setup_priority() const override { return setup_priority::LATE; }
+//   void setup() override;
+//   void dump_config() override;
+
+  void loop() override;
+
+  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
+                           esp_ble_gattc_cb_param_t *param) override;
+
 //   void update() override;
 };
 }  // namespace uni_trend_sound_meter
