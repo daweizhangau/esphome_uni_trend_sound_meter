dae8d4828fd2b64715c2e4b3c8718eb6b613f149
Dawei Zhang <daweiz@live.com>
2022-06-12 21:19:58 +1000

    minimal

--- components/uni_trend_sound_meter/__init__.py
+++ components/uni_trend_sound_meter/__init__.py
@@ -1,156 +1,27 @@
-# import esphome.codegen as cg
-# import esphome.config_validation as cv
-# from esphome.components import cover, ble_client
-# from esphome.const import CONF_ID
-
-# DEPENDENCIES = ['esp32', 'ble_client']
-
-# AUTO_LOAD = ['cover']
-# MULTI_CONF = True
-
-# CONF_CONTROLLER_ID = 'unit_trend_sound_meter_id'
-# CONF_ONLY_UP_DOWN_COMMAND = 'only_up_down_command'
-
-# sensor_namespace = cg.esphome_ns.namespace('uni_trend_sound_meter')
-
-# ControllerComponent = sensor_namespace.class_(
-#     'UnitTrendSoundMeter', cg.Component, cover.Cover, ble_client.BLEClientNode)
-
-# CONFIG_SCHEMA = cv.Schema({
-#     cv.GenerateID(): cv.declare_id(ControllerComponent),
-#     cv.Optional(CONF_ONLY_UP_DOWN_COMMAND, False): cv.boolean,
-# }).extend(ble_client.BLE_CLIENT_SCHEMA)
-
-# async def to_code(config):
-#     var = cg.new_Pvariable(config[CONF_ID])
-#     await cg.register_component(var, config)
-#     await ble_client.register_ble_node(var, config)
-
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, ble_client, esp32_ble_tracker
-from esphome.const import (
-    CONF_LAMBDA,
-    CONF_TRIGGER_ID,
-    CONF_SERVICE_UUID,
-)
-from esphome import automation
-from esphome.components.ble_client import ble_client_ns
+from esphome.components import cover, ble_client
+from esphome.const import CONF_ID
 
-DEPENDENCIES = ["ble_client"]
+DEPENDENCIES = ['esp32', 'ble_client']
 
-CONF_CHARACTERISTIC_UUID = "characteristic_uuid"
-CONF_DESCRIPTOR_UUID = "descriptor_uuid"
+AUTO_LOAD = ['cover']
+MULTI_CONF = True
 
-CONF_NOTIFY = "notify"
-CONF_ON_NOTIFY = "on_notify"
+CONF_CONTROLLER_ID = 'unit_trend_sound_meter_id'
+CONF_ONLY_UP_DOWN_COMMAND = 'only_up_down_command'
 
-adv_data_t = cg.std_vector.template(cg.uint8)
-adv_data_t_const_ref = adv_data_t.operator("ref").operator("const")
+sensor_namespace = cg.esphome_ns.namespace('uni_trend_sound_meter')
 
-BLESensor = ble_client_ns.class_(
-    "BLESensor", sensor.Sensor, cg.PollingComponent, ble_client.BLEClientNode
-)
-BLESensorNotifyTrigger = ble_client_ns.class_(
-    "BLESensorNotifyTrigger", automation.Trigger.template(cg.float_)
-)
+ControllerComponent = sensor_namespace.class_(
+    'UnitTrendSoundMeter', cg.Component, cover.Cover, ble_client.BLEClientNode)
 
-CONFIG_SCHEMA = cv.All(
-    sensor.sensor_schema(
-        BLESensor,
-        accuracy_decimals=0,
-    )
-    .extend(
-        {
-            cv.Required(CONF_SERVICE_UUID): esp32_ble_tracker.bt_uuid,
-            cv.Required(CONF_CHARACTERISTIC_UUID): esp32_ble_tracker.bt_uuid,
-            cv.Optional(CONF_DESCRIPTOR_UUID): esp32_ble_tracker.bt_uuid,
-            cv.Optional(CONF_LAMBDA): cv.returning_lambda,
-            cv.Optional(CONF_NOTIFY, default=False): cv.boolean,
-            cv.Optional(CONF_ON_NOTIFY): automation.validate_automation(
-                {
-                    cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                        BLESensorNotifyTrigger
-                    ),
-                }
-            ),
-        }
-    )
-    .extend(cv.polling_component_schema("60s"))
-    .extend(ble_client.BLE_CLIENT_SCHEMA)
-)
-
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ControllerComponent),
+    cv.Optional(CONF_ONLY_UP_DOWN_COMMAND, False): cv.boolean,
+}).extend(ble_client.BLE_CLIENT_SCHEMA)
 
 async def to_code(config):
-    var = await sensor.new_sensor(config)
-    if len(config[CONF_SERVICE_UUID]) == len(esp32_ble_tracker.bt_uuid16_format):
-        cg.add(
-            var.set_service_uuid16(esp32_ble_tracker.as_hex(config[CONF_SERVICE_UUID]))
-        )
-    elif len(config[CONF_SERVICE_UUID]) == len(esp32_ble_tracker.bt_uuid32_format):
-        cg.add(
-            var.set_service_uuid32(esp32_ble_tracker.as_hex(config[CONF_SERVICE_UUID]))
-        )
-    elif len(config[CONF_SERVICE_UUID]) == len(esp32_ble_tracker.bt_uuid128_format):
-        uuid128 = esp32_ble_tracker.as_reversed_hex_array(config[CONF_SERVICE_UUID])
-        cg.add(var.set_service_uuid128(uuid128))
-
-    if len(config[CONF_CHARACTERISTIC_UUID]) == len(esp32_ble_tracker.bt_uuid16_format):
-        cg.add(
-            var.set_char_uuid16(
-                esp32_ble_tracker.as_hex(config[CONF_CHARACTERISTIC_UUID])
-            )
-        )
-    elif len(config[CONF_CHARACTERISTIC_UUID]) == len(
-        esp32_ble_tracker.bt_uuid32_format
-    ):
-        cg.add(
-            var.set_char_uuid32(
-                esp32_ble_tracker.as_hex(config[CONF_CHARACTERISTIC_UUID])
-            )
-        )
-    elif len(config[CONF_CHARACTERISTIC_UUID]) == len(
-        esp32_ble_tracker.bt_uuid128_format
-    ):
-        uuid128 = esp32_ble_tracker.as_reversed_hex_array(
-            config[CONF_CHARACTERISTIC_UUID]
-        )
-        cg.add(var.set_char_uuid128(uuid128))
-
-    if CONF_DESCRIPTOR_UUID in config:
-        if len(config[CONF_DESCRIPTOR_UUID]) == len(esp32_ble_tracker.bt_uuid16_format):
-            cg.add(
-                var.set_descr_uuid16(
-                    esp32_ble_tracker.as_hex(config[CONF_DESCRIPTOR_UUID])
-                )
-            )
-        elif len(config[CONF_DESCRIPTOR_UUID]) == len(
-            esp32_ble_tracker.bt_uuid32_format
-        ):
-            cg.add(
-                var.set_descr_uuid32(
-                    esp32_ble_tracker.as_hex(config[CONF_DESCRIPTOR_UUID])
-                )
-            )
-        elif len(config[CONF_DESCRIPTOR_UUID]) == len(
-            esp32_ble_tracker.bt_uuid128_format
-        ):
-            uuid128 = esp32_ble_tracker.as_reversed_hex_array(
-                config[CONF_DESCRIPTOR_UUID]
-            )
-            cg.add(var.set_descr_uuid128(uuid128))
-
-    if CONF_LAMBDA in config:
-        lambda_ = await cg.process_lambda(
-            config[CONF_LAMBDA], [(adv_data_t_const_ref, "x")], return_type=cg.float_
-        )
-        cg.add(var.set_data_to_value(lambda_))
-
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await ble_client.register_ble_node(var, config)
-    cg.add(var.set_enable_notify(config[CONF_NOTIFY]))
-    for conf in config.get(CONF_ON_NOTIFY, []):
-        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-        await ble_client.register_ble_node(trigger, config)
-        await automation.build_automation(trigger, [(float, "x")], conf)
--- components/uni_trend_sound_meter/unit_trend_sound_meter.h
+++ components/uni_trend_sound_meter/unit_trend_sound_meter.h
@@ -12,7 +12,7 @@ using namespace ble_client;
 
 espbt::ESPBTUUID uuid128_from_string(std::string value);
 
-class UnitTrendSoundMeter : public Component, public BLESensor {
+class UnitTrendSoundMeter : public Component {
 //  public:
 //   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
 //                            esp_ble_gattc_cb_param_t *param) override;
