205278170fe24d9e3c4e83e502012038c16c8607
Dawei Zhang <daweiz@live.com>
2022-06-12 20:41:28 +1000

    empty BLESensor

---
+++ components/uni_trend_sound_meter/unit_trend_sound_meter.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/ble_client/sensor/ble_sensor.h"
+
+#ifdef USE_ESP32
+
+namespace esphome {
+namespace uni_trend_sound_meter {
+
+using namespace ble_client;
+
+espbt::ESPBTUUID uuid128_from_string(std::string value);
+
+class UnitTrendSoundMeter : public Component, public BLESensor {
+//  public:
+//   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
+//                            esp_ble_gattc_cb_param_t *param) override;
+//   void update() override;
+};
+}  // namespace uni_trend_sound_meter
+}  // namespace esphome
+
+#endif
