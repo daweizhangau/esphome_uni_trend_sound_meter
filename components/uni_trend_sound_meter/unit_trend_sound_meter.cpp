cb6b7f5b45a579372a1ab3475f691f6fe08c25ce
Dawei Zhang <daweiz@live.com>
2022-06-14 23:13:52 +1000

    sensor update

--- components/uni_trend_sound_meter/unit_trend_sound_meter.cpp
+++ components/uni_trend_sound_meter/unit_trend_sound_meter.cpp
@@ -112,22 +112,22 @@ void UnitTrendSoundMeter::dump_config() {
 
 
 void UnitTrendSoundMeter::update() {
-//   if (this->node_state != espbt::ClientState::ESTABLISHED) {
-//     ESP_LOGW(TAG, "[%s] Cannot poll, not connected", this->get_name().c_str());
-//     return;
-//   }
-//   if (this->handle == 0) {
-//     ESP_LOGW(TAG, "[%s] Cannot poll, no service or characteristic found", this->get_name().c_str());
-//     return;
-//   }
+  if (this->node_state != espbt::ClientState::ESTABLISHED) {
+    ESP_LOGW(TAG, "[%s] Cannot poll, not connected", this->get_name().c_str());
+    return;
+  }
+  if (this->handle == 0) {
+    ESP_LOGW(TAG, "[%s] Cannot poll, no service or characteristic found", this->get_name().c_str());
+    return;
+  }
 
-//   auto status =
-//       esp_ble_gattc_read_char(this->parent()->gattc_if, this->parent()->conn_id, this->handle, ESP_GATT_AUTH_REQ_NONE);
-//   if (status) {
-//     this->status_set_warning();
-//     this->publish_state(NAN);
-//     ESP_LOGW(TAG, "[%s] Error sending read request for sensor, status=%d", this->get_name().c_str(), status);
-//   }
+  auto status =
+      esp_ble_gattc_read_char(this->parent()->gattc_if, this->parent()->conn_id, this->handle, ESP_GATT_AUTH_REQ_NONE);
+  if (status) {
+    this->status_set_warning();
+    this->publish_state(NAN);
+    ESP_LOGW(TAG, "[%s] Error sending read request for sensor, status=%d", this->get_name().c_str(), status);
+  }
 }
 
 }  // namespace ble_client
