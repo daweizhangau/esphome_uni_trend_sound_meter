124738ce9938a7f2c226f492f43eef3dd5e98b7e
Dawei Zhang <daweiz@live.com>
2022-06-14 23:36:07 +1000

    fixup!

--- components/uni_trend_sound_meter/unit_trend_sound_meter.cpp
+++ components/uni_trend_sound_meter/unit_trend_sound_meter.cpp
@@ -23,6 +23,7 @@ void UnitTrendSoundMeter::gattc_event_handler(
     esp_gattc_cb_event_t event, 
     esp_gatt_if_t gattc_if,
     esp_ble_gattc_cb_param_t *param) {
+  ESP_LOGI(TAG, "[%s] Handling event", this->get_name().c_str());
   switch (event) {
     case ESP_GATTC_OPEN_EVT: {
       if (param->open.status == ESP_GATT_OK) {
